a17a98b1000e85df3b935ba4098089b25f36f792
Phạm Tuấn Thành <z00reyes2002@gmail.com>
2026-03-15 22:13:19 +0700

    them product batch method

--- InventoryService/src/InventoryService.Application/Interfaces/IProductBatchRepository.cs
+++ InventoryService/src/InventoryService.Application/Interfaces/IProductBatchRepository.cs
@@ -6,5 +6,6 @@ public interface IProductBatchRepository
 {
     Task<IEnumerable<ProductBatch>> GetAllAsync();
     Task<ProductBatch?> GetByIdAsync(Guid id);
+    Task<ProductBatch> AddAsync(ProductBatch batch);
     Task UpdateAsync(ProductBatch batch);
 }
