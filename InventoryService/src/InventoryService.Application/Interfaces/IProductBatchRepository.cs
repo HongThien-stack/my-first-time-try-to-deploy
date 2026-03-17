c9318264cb83a54b0e6b03752bfef68ebe1a1380
Nguyễn Sơn Hồng Thiện <sonhongthien9@gmail.com>
2026-03-17 09:54:55 +0700

    fix(IProductBatchRepository): remove duplicate AddAsync method

--- InventoryService/src/InventoryService.Application/Interfaces/IProductBatchRepository.cs
+++ InventoryService/src/InventoryService.Application/Interfaces/IProductBatchRepository.cs
@@ -7,10 +7,6 @@ public interface IProductBatchRepository
     Task<ProductBatch> AddAsync(ProductBatch batch);
     Task<IEnumerable<ProductBatch>> GetAllAsync();
     Task<ProductBatch?> GetByIdAsync(Guid id);
-<<<<<<< HEAD
     Task<IEnumerable<ProductBatch>> GetByWarehouseIdAsync(Guid warehouseId);
-    Task<ProductBatch> AddAsync(ProductBatch batch);
-=======
->>>>>>> 4c23c529ee5a6bcbf7313606c207c7c6f98a6854
     Task UpdateAsync(ProductBatch batch);
 }
