2188fc34ed51c0c04a3d1330ce269911161db3b2
Nguyễn Sơn Hồng Thiện <sonhongthien9@gmail.com>
2026-03-22 19:10:29 +0700

    fix: update Swagger configuration to support Docker environment

--- IdentityService/src/IdentityService.API/Program.cs
+++ IdentityService/src/IdentityService.API/Program.cs
@@ -115,18 +115,11 @@ builder.Services.AddScoped<IUserAuditLogRepository, UserAuditLogRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Docker"))
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Identity Service API V1");
-        c.RoutePrefix = string.Empty;
-    });
+    app.UseSwaggerUI();
 }
-
-
-
 
 app.UseCors("AllowAll");
 
